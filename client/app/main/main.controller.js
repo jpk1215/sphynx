278fd4ec17ba166c6529578fb0bf474c135d29d9
Jeffrey Klaus <jpk1215@gmail.com>
2014-12-11 18:03:22 -0500

    added transpose and cleaning helper functions for filepick

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -9,13 +9,50 @@ angular.module('sphynxApp')
       socket.syncUpdates('thing', $scope.awesomeThings);
     });
 
+    function transpose(arr) {
+      var transposed = [];
+      for (var i = 0, ii = arr[0].length; i < ii; i++) {
+        transposed.push([]);
+      };
+      for (var i = 0, ii = arr.length; i < ii; i++) {
+        for (var j = 0, jj = arr[i].length; j < jj; j++) {
+          transposed[j][i] = arr[i][j];
+        }
+      }
+      return transposed;
+    }
+
+    function clean2D (arr) {
+      var clean = [];
+      for (var i = 0, ii = arr.length; i < ii; i++) {
+        var temp = [];
+        for (var j = 0, jj= arr[i].length; j < jj; j++) {
+          if(arr[i][j].trim().length > 0) {
+            temp.push(arr[i][j].trim());
+          }
+        }
+        if(temp.length > 0){
+          clean.push(temp);
+        }
+      }
+      return clean;
+    }
+
+    function makeBostock (arr) {
+      var bostockObj = {};
+      var noNums = /\D/;
+      for (var i = 0; i < arr.length; i++) {
+        
+      };
+    } 
+
     $scope.filePick = function() {
       filepicker.setKey("Az7OkUN13Rs6HlHX403ZQz");
-
       filepicker.pick(function(Blob){
         filepicker.read(Blob, function(data){
-          console.log(Papa.parse(data))
-        })
+          var myArr = clean2D(transpose(Papa.parse(data).data));
+          console.log(myArr);   
+        });
       })
     }
 
