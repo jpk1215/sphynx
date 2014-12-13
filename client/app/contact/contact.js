f6f0f1827688cb50858ecdfee1ca2a89b1c493c4
Oddur Sigurdsson <oddurs@gmail.com>
2014-12-13 17:20:04 -0500

    added route for contact

---
+++ client/app/contact/contact.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('sphynxApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'app/contact/contact.html',
+        controller: 'ContactCtrl'
+      });
+  });
